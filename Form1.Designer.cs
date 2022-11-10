522391bb697f7759727f0e5a46531540e8cd6232
PieceOfEon <danilskripka1996@gmail.com>
2022-11-10 13:00:43 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ScrollBarProgress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 92);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 87);
+            this.textBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar1.Location = new System.Drawing.Point(207, 42);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(287, 23);
+            this.progressBar1.Step = 5;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Жмяк";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ScrollBarProgress
+{
+    public partial class Form1 : Form
+    {
+        Point p;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.MouseWheel += MyMouseWheel;
+        }
+        private void Basic_Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = new Point(e.X, e.Y);
+        }
+        private void Basic_Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - p.X;
+                Top += e.Y - p.Y;
+            }
+        }
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Read_File()
+        {
+            try
+            {
+                if (File.Exists("test.txt"))
+                    using (StreamReader F = new StreamReader("test.txt"))
+                    {
+                        textBox1.Text = F.ReadToEnd();
+                    }
+            }
+            catch (Exception E) { MessageBox.Show(E.Message); }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Read_File();
+        }
+        
+        private void MyMouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                progressBar1.Maximum = textBox1.Lines.Length-1;
+                if (progressBar1.Value + e.Delta / 40 * -1 >= progressBar1.Minimum)
+                    progressBar1.Value += e.Delta / 40 * -1;
+            }
+            else if (e.Delta < 0)
+            {
+                progressBar1.Maximum = textBox1.Lines.Length-1;
+                if (progressBar1.Value + e.Delta / 40 * -1 <= progressBar1.Maximum)
+                    progressBar1.Value += e.Delta / 40 * -1;
+            }
+        }
+    }
+}
